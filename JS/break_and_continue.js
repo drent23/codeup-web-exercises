e70de44c2b141c0d9273f82f0c6257ad447d6f04
David Renteria <david.j.renteria1@gmail.com>
2023-01-30 14:18:02 -0600

    break and continue almost done

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -1,13 +1,23 @@
-
-do while(i = 1; i <=50; i++) {
-    var oddNumber = prompt("Please provide the first odd number, between 1 and 50, that comes to mind.");
+do {
+    var oddNumber = Number(prompt("Please provide the first odd number, between 1 and 50, that comes to mind."));
     if(oddNumber % 2 === 0) {
-        prompt("That was invalid input. Please enter an odd number between 1 and 50.");
+        alert("That was an even number.  Please provide an odd number ranging from 1 to 50.");
+    } else if(oddNumber < 0 || oddNumber > 50) {
+        alert("That number was either a negative number or greater than 50.  Please provide an odd number ranging from 1 to 50.");
+    } else if(isNaN(oddNumber)) {
+        alert("This is not a number.  Please provide an odd number fanging from 1 to 50.");
+    } else {
+        alert("Thank you for the valid number.");
+    }
         break;
-    } if(oddNumber % 2 !== 0) {
-        console.log("Number to skip is: " + oddNumber);
+}   while (true);
 
-        console.log("Here is an odd number: " + i);
+for(var i = 1; i <= 50; i++) {
+    if( i % 2 === 0) {
         continue;
+    } else if( i === oddNumber) {
+        console.log("Yikes! Skipping number:" + oddNumber);
+    } else {
+        console.log("Here is an odd number: " + i);
     }
 }
