18c1affa8fa285f7b4eb5cda8c2bd1aaf5b00a68
David Renteria <david.j.renteria1@gmail.com>
2023-01-30 15:31:00 -0600

    still a work in progress but loop exercises complete

--- JS/while.js
+++ JS/while.js
@@ -3,3 +3,21 @@ while (i <= 16) {
     console.log("2" ** i);
     i++;
 }
+/* the code in the do block will get executed once before the while condition is checked. The condition is then checked to decide if the loop should continue. For the above example, the do block will only get executed once
+ */
+
+
+var conesToSell = Math.floor((Math.random() * 50) + 50);
+alert("You have " + conesToSell + " cones to sell today.");
+do {
+    var howManyCones = Math.floor((Math.random() * 5) + 1);
+    if (howManyCones > conesToSell) {
+       console.log("Cannot sell you " + howManyCones + " cones because I only have " + conesToSell + " left.");
+    } else {
+        conesToSell -= howManyCones;
+        console.log(howManyCones + " cones sold.");
+        console.log(conesToSell + " cones are left.");
+    }
+} while (conesToSell > 0);
+    console.log("Hooray!  We're sold out!");
+
