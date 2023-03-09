de29b1e235955e46186ebe70729e167560010b12
David Renteria <david.j.renteria1@gmail.com>
2023-03-09 09:00:42 -0600

    still working but complete

--- JS/jquery_faq.js
+++ JS/jquery_faq.js
@@ -3,3 +3,21 @@
 $('#hiddenLink').click(function(){
     $('dd').toggleClass('invisible');
 })
+
+
+$('#liYellow').click(function () {
+    $("ul").each(function() {
+        $(this).children().last().css('background-color', 'yellow')
+})})
+$('h3').click(function () {
+    $("ul").each(function() {
+        $(this).children().css('font-weight', 'bold')
+    })})
+$('li').click(function () {
+    $("ul").each(function() {
+        $(this).children().first().css('color', '#00F')
+    })})
+
+
+//
+//     When any list item is clicked, first li of the parent ul should have a font color of blue.
