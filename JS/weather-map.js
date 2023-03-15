70629a2d1829990ad4cdc43463b47dc7558f0b68
David Renteria <david.j.renteria1@gmail.com>
2023-03-15 06:41:37 -0500

    Still working

--- JS/weather-map.js
+++ JS/weather-map.js
@@ -31,3 +31,83 @@
         weatherData($('#citySearch').val());
     })
 })();
+let gotPosition = function(pos) {
+    let lat = pos.coords.latitude;
+    let long = pos.coords.longitude;
+    getForecast(lat, long);
+}
+let getForecast = function(lat, long) {
+    let url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&exclude=current,minutely,hourly&appid=${weatherKey}&units=imperial";
+    getWeatherText(url);
+}
+async function getWeatherText(url) {
+    let weatherObject = await fetch(url);
+    let weatherText = await weatherObject.text();
+    console.log(weatherObject);
+    console.log(weatherText);
+    parseWeather(weatherText);
+}
+let parseWeather = function(weatherText) {
+    let weatherJSON = JSON.parse(weatherText);
+    console.log(weatherJSON);
+    let dailyForecast = weatherJSON.daily;
+    console.log(dailyForecast);
+    for(x=0; x<dailyForecast.length; x++) {
+        let day = dailyForecast[x];
+        let today = new Date().getDay() + x;
+        if(today > 6) {
+            today = today - 7;
+        }
+        let dayOfWeek = getDayOfWeek(today);
+        let description = day.weather[0].description;
+        let icon = day.weather[0].icon;
+        let sunrise = timestampToTime(day.sunrise);
+        let sunset = timestampToTime(day.sunset);
+        let highTemp = kToF(day.temp.max);
+        let lowTemp = kToF(day.temp.min);
+        let humidity = day.humidity;
+        let windSpeed = day.wind_speed;
+        let windGust = day.wind_gust;
+        displayWeatherDay(dayOfWeek, description, icon, sunrise, sunset, highTemp, lowTemp, humidity, windSpeed, windGust);
+    }
+}
+let displayWeatherDay = function(dayOfWeek, description, icon, sunrise, sunset, highTemp, lowTemp, humidity, windSpeed, windGust) {
+    let fiveDayHTML = "<div class='dailyWeather'><img src='http://openweathermap.org/img/wn/" + icon + ".png'/>";
+    fiveDayHTML += "<h2>" + dayOfWeek + "</h2>";
+    fiveDayHTML += "<h3>" + description + "</h3>";
+    fiveDayHTML += "<h6>Sunrise: " + sunrise + "</h6>";
+    fiveDayHTML += "<h6>Sunset: " + sunset + "</h6>";
+    fiveDayHTML += "<h6>High: " + highTemp + "</h6>";
+    fiveDayHTML += "<h6>Low: " + lowTemp + "</h6>";
+    fiveDayHTML += "<h6>Humidity: " + humidity + "%</h6>";
+    fiveDayHTML += "<h6>Winds: " + parseInt(windSpeed) + " with gusts up to " + parseInt(windGust) + " knots</h6></div>";
+    document.getElementById("forecast").innerHTML +=fiveDayHTML;
+    }
+let getDayOfWeek = function(whichDay) {
+    let weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    return (weekday[whichDay]);
+}
+let kToF = function(kelvinToFahr) {
+    const celsius = kelvinToFahr - 273;
+    const fahrenheit = Math.floor(celsius * (9/5) + 32);
+    return fahrenheit
+}
+let timestampToTime = function(timeStamp) {
+    let date = new Date(timeStamp * 1000);
+    let hours = date.getHours();
+    let minutes = "";
+    if(date.getMinutes() < 10) {
+        minutes = "0" + date.getMinutes();
+    } else {
+        minutes = date.getMinutes();
+    }
+    return hours + ":" + minutes;
+}
+navigator.geolocation.getCurrentPosition(gotPosition);
