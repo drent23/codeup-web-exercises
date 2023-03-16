ca845aaf713ff6e53e454b3f83e879b97bbb82e3
David Renteria <david.j.renteria1@gmail.com>
2023-03-16 14:07:52 -0500

    few more changes

--- JS/weather-map.js
+++ JS/weather-map.js
@@ -1,5 +1,8 @@
 (function(){
     "use strict";
+
+    //map
+
     mapboxgl.accessToken = myMapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
@@ -7,6 +10,16 @@
         center: [-74.5, 40], // starting position [lng, lat]
         zoom: 9, // starting zoom
     });
+    map.addControl(new mapboxgl.GeolocateControl({
+        positionOptions:{
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true,
+        showUserHeading: true
+    }));
+
+    //marker
+
     let weatherMarker = new mapboxgl.Marker({draggable: true})
         .setLngLat([-74.5, 40])
         .addTo(map);
@@ -16,11 +29,12 @@
      let arrWeather = [lngLat.lng, lngLat.lat];
       console.log(lngLat);
       weatherData(arrWeather)
-         weatherForecast(arrWeather)
          fiveDayForecast(arrWeather)
-
      }
      weatherMarker.on(`dragend`, onDragEnd);
+
+     //goecode
+
      function getLocation(searchString) {
          geocode(searchString, myMapToken).then(function (results) {
              weatherMarker.setLngLat(results);
@@ -30,9 +44,12 @@
              fiveDayForecast(results);
                   })
              };
+
+     //current weather
+
      function weatherData(results) {
-         let html = "";
          $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${results[1]}&lon=${results[0]}&appid=${weatherKey}&units=imperial`).done(function (data) {
+             let html = "";
              html += `<h6>Date: ${dateConversion(data.dt)}</h6>`;
              html += `<h6>City: ${data.name}</h6>`;
              html += `<h6>Weather: ${data.weather[0].description}</h6>`;
@@ -42,16 +59,32 @@
              $("#currentCard").html(html);
          })
      };
-    // function weatherForecast(results) {
-    //     $.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${results[1]}&lon=${results[0]}&exclude=current,minutely,hourly&appid=${weatherKey}&units=imperial`).done(function (data) {
-    //         let html = '';
-            // // for(function () {
 
-            // })
+     //five-day forecast
+
+    function fiveDayForecast(results) {
+        $.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${results[1]}&lon=${results[0]}&exclude=current,minutely,hourly&appid=${weatherKey}&units=imperial`).done(function(data){
+            let html = '';
+            for (let i = 1; i < 6; i++) {
+                const newDate = new Date(data.daily[i].dt * 1000);
+                html += `<h2>Date: ${newDate.toDateString()}</h2>`;
+                html += `<h6>Weather: ${data.daily[i].weather[0].description}</h6>`;
+                html += `<h6>Temp: ${data.daily[i].temp.day} &deg;</h6>`;
+                html += `<h6>Humidity: ${data.daily[i].humidity}</h6>`;
+            }
+            $('#cardGroup').html(html)
+        })
+    }
+
+    //input search and submit botton
+
             $('#searchBtn').click(function (e) {
                 e.preventDefault();
                 getLocation($('#citySearch').val());
             });
+
+    //dt conversion
+
             let dateConversion = function (timeStamp) {
                 let date = new Date(timeStamp * 1000).toLocaleString("en-US", {
                     weekday: "short",
@@ -60,27 +93,23 @@
                 });
                 return date;
             }
-        // })
-    //     var forecast = document.querySelector('#cardGroup');
-    //     forecast.innerHTML = weatherForecast(fiveDays);
-    // }
 })();
+
+//notes for future reference
+
 // let gotPosition = function(pos) {
 //     let lat = pos.coords.latitude;
 //     let long = pos.coords.longitude;
-//     getForecast(lat, long);
-// }
+//     getForecast(lat, long);}
 // let getForecast = function(lat, long) {
 //     let url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&exclude=current,minutely,hourly&appid=${weatherKey}&units=imperial";
-//     getWeatherText(url);
-// }
+//     getWeatherText(url);}
 // async function getWeatherText(url) {
 //     let weatherObject = await fetch(url);
 //     let weatherText = await weatherObject.text();
 //     console.log(weatherObject);
 //     console.log(weatherText);
-//     parseWeather(weatherText);
-// }
+//     parseWeather(weatherText);}
 // let parseWeather = function(weatherText) {
 //     let weatherJSON = JSON.parse(weatherText);
 //     console.log(weatherJSON);
@@ -90,8 +119,7 @@
 //         let day = dailyForecast[x];
 //         let today = new Date().getDay() + x;
 //         if(today > 6) {
-//             today = today - 7;
-//         }
+//             today = today - 7;}
 //         let dayOfWeek = getDayOfWeek(today);
 //         let description = day.weather[0].description;
 //         let icon = day.weather[0].icon;
@@ -102,9 +130,7 @@
 //         let humidity = day.humidity;
 //         let windSpeed = day.wind_speed;
 //         let windGust = day.wind_gust;
-//         displayWeatherDay(dayOfWeek, description, icon, sunrise, sunset, highTemp, lowTemp, humidity, windSpeed, windGust);
-//     }
-// }
+//         displayWeatherDay(dayOfWeek, description, icon, sunrise, sunset, highTemp, lowTemp, humidity, windSpeed, windGust);}}
 // let displayWeatherDay = function(dayOfWeek, description, icon, sunrise, sunset, highTemp, lowTemp, humidity, windSpeed, windGust) {
 //     let fiveDayHTML = "<div class='dailyWeather'><img src='http://openweathermap.org/img/wn/" + icon + ".png'/>";
 //     fiveDayHTML += "<h2>" + dayOfWeek + "</h2>";
@@ -115,8 +141,7 @@
 //     fiveDayHTML += "<h6>Low: " + lowTemp + "</h6>";
 //     fiveDayHTML += "<h6>Humidity: " + humidity + "%</h6>";
 //     fiveDayHTML += "<h6>Winds: " + parseInt(windSpeed) + " with gusts up to " + parseInt(windGust) + " knots</h6></div>";
-//     document.getElementById("forecast").innerHTML +=fiveDayHTML;
-//     }
+//     document.getElementById("forecast").innerHTML +=fiveDayHTML;}
 // let getDayOfWeek = function(whichDay) {
 //     let weekday = new Array(7);
 //     weekday[0] = "Sunday";
@@ -126,13 +151,11 @@
 //     weekday[4] = "Thursday";
 //     weekday[5] = "Friday";
 //     weekday[6] = "Saturday";
-//     return (weekday[whichDay]);
-// }
+//     return (weekday[whichDay]);}
 // let kToF = function(kelvinToFahr) {
 //     const celsius = kelvinToFahr - 273;
 //     const fahrenheit = Math.floor(celsius * (9/5) + 32);
-//     return fahrenheit
-// }
+//     return fahrenheit}
 // let timestampToTime = function(timeStamp) {
 //     let date = new Date(timeStamp * 1000);
 //     let hours = date.getHours();
@@ -142,28 +165,5 @@
 //     } else {
 //         minutes = date.getMinutes();
 //     }
-//     return hours + ":" + minutes;
-// }
+//     return hours + ":" + minutes;}
 // navigator.geolocation.getCurrentPosition(gotPosition);
-function fiveDayForecast(results) {
-    $.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${results[1]}&lon=${results[0]}&exclude=current,minutely,hourly&appid=${weatherKey}&units=imperial`).done(function(data){
-        const fiveDays = [];
-        let html = '';
-        for (let i = 1; i < 5; i++) {
-            const newDate = new Date(data.daily[i].dt * 1000);
-            const newDay = {
-                date: newDate.toDateString(),
-                temp: data.daily[i].temp.day,
-                humidity: data.daily[i].humidity,
-                condition: data.daily[i].weather.description
-            };
-            html += `<h6>Date: ${newDate.toDateString()}</h6>`;
-            html += `<h6>Weather: ${data.daily[i].weather.description}</h6>`;
-            html += `<h6>Temp: ${data.daily[i].temp.day} &deg;</h6>`;
-            html += `<h6>Humidity: ${data.daily[i].humidity}</h6>`;
-        }
-        $('#cardGroup').html(html)
-        // fiveDays.push(newDay);
-        console.log(fiveDays);
-    })
-}
